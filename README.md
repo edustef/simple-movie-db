380cf8b6df373b2b545e857d04c079a935c02ca8
Stefan Eduard <eduardstefan290@gmail.com>
2020-12-27 20:54:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tailwind-template
+
+Simple react template with tailwindcss with PurgeCSS ready for production.
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='text-5xl font-semibold grid place-items-center h-screen'>
+      <h1>React TailwindCSS Boilerplate</h1>
+    </div>
+  );
+}
+
+export default App;
